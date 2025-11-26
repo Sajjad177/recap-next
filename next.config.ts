9cad50b345348ca6b57bf39a83b85530c2d9b5fb
Sajjad177 <sajjadhossainx06@gmail.com>
2025-11-27 04:41:49 +0600

    recap next.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,41 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    root: path.join(__dirname),
+    // loader rules
+    rules: {
+      // SVG -> React component
+      "*.svg": {
+        loaders: [
+          {
+            loader: "@svgr/webpack",
+            options: { icon: true },
+          },
+        ],
+        as: "*.js",
+      },
+      // MDX example (if you set up mdx loader)
+      "*.mdx": {
+        loaders: ["@mdx-js/loader"],
+        as: "*.js",
+      },
+    },
+    resolveAlias: {
+      // alias underscore -> lodash
+      underscore: "lodash",
+      // conditional alias example
+      mocha: { browser: "mocha/browser-entry.js" },
+    },
+    resolveExtensions: [".mdx", ".tsx", ".ts", ".jsx", ".js", ".mjs", ".json"],
+    debugIds: false,
+  },
+  // other Next config
+  reactStrictMode: true,
+  images: {
+    domains: ["localhost", "res.cloudinary.com"],
+  },
 };
 
 export default nextConfig;
